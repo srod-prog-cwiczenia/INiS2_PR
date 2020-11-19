70eefee8045c72f114e08c175330915eb513a10c
Andrzej Nowik <andrzej@mat.ug.edu.pl>
2020-11-19 12:31:29 +0100

    konstruktor do Osoba

--- define.h
+++ define.h
@@ -17,5 +17,10 @@ struct Osoba {
 	struct Adres {
 		string ulica;
 		unsigned int nr_domu;
-	};
+		Adres(string ulica_p, unsigned int nr_domu_p) :
+			ulica(ulica_p), nr_domu(nr_domu_p) {};
+	} adr;
+	Osoba(string imie_p, string nazwisko_p, string ulica_p, unsigned int nr_domu_p) :
+		imie(imie_p), nazwisko(nazwisko_p), adr(ulica_p, nr_domu_p)
+	{};
 };
